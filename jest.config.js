9445f53b4a4afe124e4263548e4f9c048cc40e8d
Parik Maan <25786988+parikmaan@users.noreply.github.com>
2025-06-15 10:20:37 -0700

    Changed Jest coverage folder location

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
+  rootDir: '.',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.ts$': 'ts-jest',
